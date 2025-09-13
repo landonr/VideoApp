dece66aa15470eb0c874f3c8786e41e4a502e5c3
landon rohatensky <landonroha@gmail.com>
2025-09-13 13:37:11 -0700

    Initial Commit

---
+++ VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by Landon Rohatensky on 2025-09-13.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoAppTests/VideoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppTests.swift
+//  VideoAppTests
+//
+//  Created by Landon Rohatensky on 2025-09-13.
+//
+
+import Testing
+@testable import VideoApp
+
+struct VideoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
